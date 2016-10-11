de8d19c721e0288b4e884b79c695ff3be99b1d47
Ken Li <iliawnek@gmail.com>
2016-10-11 06:27:26 -0700

    Broken solution to exercise 3

--- findmain.c
+++ findmain.c
@@ -1,51 +1,46 @@
 #include <stdio.h>
 #include "string.h"
 #define MAXLINE 1000 /* maximum input line length */
-#define USAGE "Usage: ./find [-x] pattern\n" /* message when incorrectly used */
+#define USAGE "Usage: ./find [-x] pattern [pattern] ...\n" /* message when incorrectly used */
 #define TRUE 1
 #define FALSE 0
 
 /* print all lines from standard input that match pattern */
 int main(int argc, char* argv[]) {
-    /* detect incorrect number of arguments */
-    if (argc < 2 || argc > 3) {
+    /* error if no args found */
+    if (argc < 2) {
+        printf("Error: No arguments.\n");
         printf(USAGE);
         return -1;
     }
 
-    char* pattern; /* pattern to search for */
+    /* check for flag */
     int exclude = FALSE;
-
-    if (argc == 2) {
-        /* error if only flag is given */
-        if (strcmp(argv[1], "-x") == 0) {
+    if (strcmp(argv[1], "-x") == 0) { /* if flag is present */
+        if (argc < 3) { /* error if only flag is given */
+            printf("Error: No pattern(s).\n");
             printf(USAGE);
-            return -2;
-        }
-        pattern = argv[1];
-    }
-    else if (argc == 3) {
-        /* error if 1st of 2 args is not flag */
-        if (strcmp(argv[1], "-x") != 0) {
-            printf(USAGE);
-            return -3;
+            return -1;
         }
         exclude = TRUE;
-        pattern = argv[2];
     }
 
     char line[MAXLINE];
     int found = 0;
+    char* pattern;
     while (fgets(line, MAXLINE, stdin) != NULL) {
-        if (strstr(line, pattern) != NULL) {
-            if (!exclude) {
+        for (int i = exclude ? 2 : 1; i++; i < argc) {
+            printf("test\n");
+            pattern = argv[i];
+            if (strstr(line, pattern) != NULL) {
+                if (!exclude) {
+                    printf("%s", line);
+                }
+                found++;
+            } else if (exclude) {
                 printf("%s", line);
             }
-            found++;
-        } else if (exclude) {
-            printf("%s", line);
         }
-
     }
     return found;
 }
