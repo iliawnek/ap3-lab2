2c58199948aadd7154b2bd6fdfd5c967dcdb4ebb
Ken Li <iliawnek@gmail.com>
2016-10-11 17:00:03 +0100

    Fix findmain.c for exercise 3

--- findmain.c
+++ findmain.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "string.h"
+
 #define MAXLINE 1000 /* maximum input line length */
 #define USAGE "Usage: ./find [-x] pattern [pattern] ...\n" /* message when incorrectly used */
 #define TRUE 1
 #define FALSE 0
 
 /* print all lines from standard input that match pattern */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     /* error if no args found */
     if (argc < 2) {
         printf("Error: No arguments.\n");
@@ -17,8 +18,9 @@ int main(int argc, char* argv[]) {
     /* check for flag */
     int exclude = FALSE;
     if (strcmp(argv[1], "-x") == 0) { /* if flag is present */
-        if (argc < 3) { /* error if only flag is given */
-            printf("Error: No pattern(s).\n");
+        /* error if only flag is given */
+        if (argc < 3) {
+            printf("Error: No patterns.\n");
             printf(USAGE);
             return -1;
         }
@@ -27,18 +29,19 @@ int main(int argc, char* argv[]) {
 
     char line[MAXLINE];
     int found = 0;
-    char* pattern;
+    char *pattern;
+    int i;
     while (fgets(line, MAXLINE, stdin) != NULL) {
-        for (int i = exclude ? 2 : 1; i++; i < argc) {
-            printf("test\n");
+        for (i = exclude ? 2 : 1; i < argc; i++) {
             pattern = argv[i];
             if (strstr(line, pattern) != NULL) {
                 if (!exclude) {
                     printf("%s", line);
                 }
                 found++;
-            } else if (exclude) {
-                printf("%s", line);
+                break; // skip to next line once first match is found
+            } else if (exclude && i == argc - 1) {
+                printf("%s", line); // only print if pattern is last argument
             }
         }
     }
