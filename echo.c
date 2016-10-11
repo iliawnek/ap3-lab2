8ef7b7ce0fd27d371f4211548c04448eaa9e2b47
Ken Li <iliawnek@gmail.com>
2016-10-11 17:54:28 +0100

    Exercise 3.1

--- echo.c
+++ echo.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
+    int skip = 0; // if true, current arg is skipped
+
     for (int i = 0; i < argc; i++) {
+        // skip and reset
+        if (skip) {
+            skip = 0;
+            continue;
+        }
+        // skip "-diag level"
+        else if (strcmp(argv[i], "-diag") == 0) {
+            // error if no flag is given to -diag
+            if (i == argc - 1) {
+                printf("\nError: No argument given to -diag.\n");
+                return -1;
+            }
+            skip = 1;
+            continue;
+        }
         printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
     }
     printf("\n");
